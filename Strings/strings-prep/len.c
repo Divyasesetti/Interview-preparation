480b3bc83b3c62ccc2bda919518b257b7e64d41e
Divyasesetti <36499814+Divyasesetti@users.noreply.github.com>
2018-11-01 03:00:15 +0000

    Update len.c

--- Strings/strings-prep/len.c
+++ Strings/strings-prep/len.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 int main(void)
 {
-    char str[10],i,length=0;
+    char str[10];
+    int i,length=0;
     printf("enter astring");
     scanf("%s",str);
     for(i=0;str[i]!='\0';++i)
